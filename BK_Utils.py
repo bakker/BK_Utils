76f9048886ae23a4fd953ac814c8b3a90fb8c022
bakker <bart@bartkestelyn.com>
2025-12-01 22:19:23 +0100

    Add IsOneOfGroupsActive class to BK_Utils.py

--- BK_Utils.py
+++ BK_Utils.py
@@ -459,6 +459,49 @@ class SamePixelResolutionCalculator:
 
         return (W, H)
 
+import math
+
+class IsOneOfGroupsActive:
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "required": {
+                "group_name_contains": (
+                    "STRING",
+                    {
+                        "default": "",
+                        "multiline": False,
+                    },
+                ),
+                "active_state": (
+                    "BOOLEAN",
+                    {
+                        "default": False,
+                    },
+                ),
+            }
+        }
+
+    RETURN_TYPES = ("BOOLEAN",)
+    RETURN_NAMES = ("boolean",)
+    FUNCTION = "pass_state"
+    CATEGORY = "utils/logic"
+
+    @classmethod
+    def IS_CHANGED(cls, group_name_contains, active_state):
+        return float("nan")
+
+    def pass_state(self, group_name_contains, active_state):
+        return (active_state,)
+
+
+NODE_CLASS_MAPPINGS = {
+    "IsOneOfGroupsActive": IsOneOfGroupsActive,
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "IsOneOfGroupsActive": "IsOneOfGroupsActive",
+}
 
 # Register node for ComfyUI
 NODE_CLASS_MAPPINGS = {
